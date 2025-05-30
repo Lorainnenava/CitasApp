6fcb16b8dda89ffe80d3d9e8c4d41a830ffad04b
Lorainnenava <lorainnenavarro0723@gmail.com>
2025-05-30 17:04:08 -0500

    Creando la entidad de Modules y SubModules

---
+++ MyApp.Domain/Entities/ModulesEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace MyApp.Domain.Entities
+{
+    public class ModulesEntity
+    {
+        public int ModuleId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Icon { get; set; } = string.Empty;
+        public string Route { get; set; } = string.Empty;
+        public int Order { get; set; }
+
+        // Relaciones
+        public ICollection<SubModulesEntity> SubModules { get; set; } = new List<SubModulesEntity>();
+    }
+}
---
+++ MyApp.Domain/Entities/SubModulesEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace MyApp.Domain.Entities
+{
+    public class SubModulesEntity
+    {
+        public int SubModuleId { get; set; }
+        public int ModuleId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Route { get; set; } = string.Empty;
+        public int Order { get; set; }
+
+        // Relaciones
+        public ModulesEntity Module { get; set; } = null!;
+    }
+}
